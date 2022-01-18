14306e54eb73c34e6cc93a771c0f961e1d40e287
Kirill Danilov <kirilldanilov@Kirills-MacBook-Pro.local>
2022-01-18 15:06:38 +0300

    Bugfixes

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -20,31 +20,35 @@ const getCookie = function(name) {
 };
 
 let downloadTimeout;
+let trigger;
 const checkDownloadCookie = function() {
     if (getCookie("downloadStarted") === "1") {
         setCookie("downloadStarted", 1, 1000); //Expiration could be anything... As long as we reset the value
         document.getElementById('dimmer').style.display='none';
         removeCustomAlert();
-        console.log('Cookie downloadStarted=1 was set and screen must become active')
+        console.log('Cookie downloadStarted=1 was retrieved and screen must become active')
+        trigger = true
     } else if (getCookie("conformityFailed") === "1") {
         setCookie("downloadStarted", 0, 1000);
         document.getElementById('dimmer').style.display='none';
         removeCustomAlert();
-        console.log('Cookie downloadStarted=0 was set and screen must become active')
+        console.log('Cookie conformityFailed=1 was retrieved and screen must become active')
         createCustomAlert('Oops! The provided file did not meet the conformity criteria. Please, check your file and try again.')
+        trigger = true
     } else {
-        downloadTimeout = setTimeout(checkDownloadCookie, 500); //Re-run this function in 1 second.
+        trigger = false
+        downloadTimeout = setTimeout(checkDownloadCookie, 500); //Re-run this function
     }
 };
 
 const cookieSettle = function() {
     console.log('cookieSettle initiated')
     setCookie('downloadStarted', 0, 60 * 1000);
-    setTimeout(checkDownloadCookie, 500);
+    checkDownloadCookie();
 }
 
 const relaxWindow = function() {
-    console.log('Got an error in iframe, relaxing screen now')
+    console.log('Got a change in HTTP response iframe, relaxing screen now')
     document.getElementById('dimmer').style.display='none';
     clearTimeout(downloadTimeout)
 }
