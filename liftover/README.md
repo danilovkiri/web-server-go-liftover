ee7fa1335efa07123f7e060291fbd70ff52c25ef
Kirill Danilov <danilov@atlas.ru>
2022-02-10 10:38:30 +0300

    Code refactoring

--- liftover/README.md
+++ liftover/README.md
@@ -1,30 +1,17 @@
 # 23andme Data Liftover hg38/hg19
 
 ## Description
-The project can be used for positional and optionally allelic liftover between the GRCh38 and GRCh37 builds. Positional
-lifover may be accompanied by reverse-complement difference detection, which will trigger an allelic complementary change.
-This option is time-consuming resulting in over x1000 speed reduction.
+The project can be used for positional liftover between the GRCh38 and GRCh37 builds. 
 
 ## Dependencies
 1. [pyliftover](https://github.com/konstantint/pyliftover) (necessary contents are included)
 2. UCSC liftover chain files (included), download is available for [hg19]([http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/)
    and [hg38](http://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/)
-3. `twoBitTwoFa_linux` and `twoBitTwoFa_macos` shell scripts, available from [here](http://hgdownload.soe.ucsc.edu/admin/exe/)),
-   should be accessible from main project directory and have the above-specified name (might be necessary to change file
-   permissions to run by `sudo chmod -R 777 <file>`); included in the project
-4. `hg38.2bit` genome sequence file for fast access (can be downloaded from UCSC repository
-   [here](http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/)), should be accessible from main project directory and
-   have the above-specified name
-5. `hg19.2bit` genome sequence file for fast access (can be downloaded from UCSC repository
-   [here](http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/)), should be accessible from main project directory and
-   have the above-specified name
 6. Python 3+
 
 ## Usage
-Run the `main.py` passing `--input`, `--output` and `--source` arguments, where the `--source` denotes the genome build
-of the input file. You can optionally use `--fast` argument with the value `True` for fast conversion (disables allelic
-check).
+Run the `main.py` passing passing input, output text files and genome source build (either `hg38` or `hg19`).
 
 ```bash
-python3 main.py --input input.txt --output output.txt --source hg38 --fast True
+python3 main.py <INPUT> <OUTPUT> <hg38|hg19>
 ```
--- liftover/main.py
+++ liftover/main.py
@@ -4,39 +4,22 @@ import csv
 import os
 from pyliftover.liftover import LiftOver
 import sys
-import datetime
-import numpy as np
-import utils as utils
 import warnings
 warnings.filterwarnings('ignore')
 
 
 def main(file_in, file_out, source_build):
-    os_platform = utils.check_system()
     project_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '../'))
     if source_build == 'hg19':
         lo = LiftOver('{0}/hg19ToHg38.over.chain.gz'.format(project_dir))
     elif source_build == 'hg38':
         lo = LiftOver('{0}/hg38ToHg19.over.chain.gz'.format(project_dir))
     else:
-        print('### ERROR: Incorrect source genome build')
-        sys.exit(1)
+        raise ValueError("Incorrect source genome build")
     with open(file_in, 'r') as f_in, open(file_out, 'w+') as f_out:
         f_out.write('\t'.join(map(str, ['# rsid', 'chromosome', 'position', 'genotype'])) + '\n')
         reader = csv.reader(f_in, delimiter='\t')
-        count = 1
-        subtime0 = datetime.datetime.now()
-        subtime1 = datetime.datetime.now()
         for i in reader:
-            if count % 100000 == 0:
-                subtime2 = datetime.datetime.now()
-                diftime = subtime2 - subtime1
-                difsec = diftime.seconds
-                diftime0 = subtime2 - subtime0
-                difmin0 = np.round(divmod(diftime0.seconds, 60)[0] + (divmod(diftime0.seconds, 60)[1] / 60), 2)
-                print('### INFO: Progress: {0}\tLast 100000: {1} sec\tElapsed: {2} min'.format(count, difsec, difmin0))
-                subtime1 = datetime.datetime.now()
-            count += 1
             if i[0][0] == '#':
                 continue
             else:
@@ -57,33 +40,7 @@ def main(file_in, file_out, source_build):
                             pos_lo = int(list(lo.convert_coordinate('chr{0}'.format(chr_id), pos - 1, '+')[0])[1]) + 1
                         except:
                             continue
-                    vartype = utils.get_vartype(gt)
-                    if vartype == 'INDEL':
-                        f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, gt])) + '\n')
-                    elif vartype == 'SNV':
-                        if fast_mode is True:
-                            f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, gt])) + '\n')
-                        else:
-                            if source_build == 'hg19':
-                                seq_hg19 = utils.query_dna(chr_id, pos-4, pos+4, project_dir, 'hg19', os_platform)
-                                seq_hg38 = utils.query_dna(chr_id, pos_lo-4, pos_lo+4, project_dir, 'hg38', os_platform)
-                                allele_hg19 = seq_hg19[4]
-                                allele_hg38 = seq_hg38[4]
-                                trigger = utils.check_complement(seq_hg19, seq_hg38)
-                                if trigger is False:
-                                    f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, gt])) + '\n')
-                                elif trigger is True:
-                                    f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, utils.make_complement(gt)])) + '\n')
-                            elif source_build == 'hg38':
-                                seq_hg19 = utils.query_dna(chr_id, pos_lo-4, pos_lo+4, project_dir, 'hg19', os_platform)
-                                seq_hg38 = utils.query_dna(chr_id, pos-4, pos+4, project_dir, 'hg38', os_platform)
-                                allele_hg19 = seq_hg19[4]
-                                allele_hg38 = seq_hg38[4]
-                                trigger = utils.check_complement(seq_hg19, seq_hg38)
-                                if trigger is False:
-                                    f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, gt])) + '\n')
-                                elif trigger is True:
-                                    f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, utils.make_complement(gt)])) + '\n')
+                    f_out.write('\t'.join(map(str, [snp_id, chr_id, pos_lo, gt])) + '\n')
     f_in.close()
     f_out.close()
     return None
@@ -93,5 +50,4 @@ if __name__ == '__main__':
     file_in = str(sys.argv[1])
     file_out = str(sys.argv[2])
     source_build = str(sys.argv[3])
-    fast_mode = True
     main(file_in, file_out, source_build)
